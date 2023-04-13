623dd5440af67e0c029aa0f698a1655773d1539c
Bahodir99 <bahodirataboyev7@gmail.com>
2023-04-13 23:21:54 +0500

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Signin from './Login page/Signin/Signin';
+import Signup from './Login page/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <Signin/> */}
+      <Routes>
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </div>
   );
 }
