9354bd71981347cd21f408b9c44b62a3e108b7f1
Bahodir99 <bahodirataboyev7@gmail.com>
2023-04-14 16:09:15 +0500

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import SignUp from './Login page/SignUp/SignUp';
 import Header from './components/Header/Header';
-import Signin from './Login page/SignIn/SignIn';
+import Signin from './Login/Signin/Signin';
+import Signup from './Login/Signup/Signup';
+
 
 function App() {
   return (
     <div className="App">
+      
       <Routes>
         <Route path='/signin' element={<Signin />} />
-        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signup' element={<Signup />} />
       </Routes>
       <Header />
     </div>
