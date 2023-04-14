1a6237ffaa13ccb9fcd98eb8558f8e5c9c4e3d44
Usubjonovshuxratbek <usubjonovshuxratbek1@gmail.com>
2023-04-14 13:46:10 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { Route, Routes } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Blog from './pages/Blog/Blog';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [product, setProduct] = useState([])
+    useEffect(() => {
+        axios.get('https://63df32c43d94d02c0bb5c567.mockapi.io/posts')
+            .then(function (response) {
+                // handle success
+                console.log(response);
+                setProduct(response.data)
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+    }, [])
   return (
     <div className="App">
       <Header />
+      
+      <Routes>
+        <Route path='/' element={<Home products={product}/>} />
+        <Route path='/blog' element={<Blog products={product}/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeMain from '../../components/Home-Main/Home-Main'
+
+function Home({products}) {
+  console.log(products);
+  return (
+    <div>
+      <HomeMain />
+    </div>
+  )
+}
+
+export default Home
