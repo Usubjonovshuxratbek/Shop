389e0699a46737a375e2117adf257a73bdfe4844
Bahodir99 <bahodirataboyev7@gmail.com>
2023-04-18 18:51:51 +0500

    find

--- src/Login/Signin/Signin.jsx
+++ src/Login/Signin/Signin.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import './Signin.scss'
+import { usersLogin, usersPassword } from '../FullData'
+
+
+
+
 function Signin() {
+    const [user , setUser] = useState()
     const navigate = useNavigate()
+    const upRef = ()=>{
+        navigate('/signup')
+    }
+  useEffect(()=>{
+    fetch('https://63e542a8c04baebbcdb7065c.mockapi.io/Data')
+    .then((res)=> res.json())
+    .then((data)=> setUser(data))
+  }, [user])
+  
+    const fn = (e) => {
+        e.preventDefault()
+        let a = e.target.text.value
+        let b = e.target.password.value
+        // if(usersLogin == a && usersPassword == b){
+        //     // navigate('/')
+        //     console.log('ok');
+        // } 
+        user.map((i)=>{
+            if(i.Email == a && i.Password == b){
+               navigate('/')
+            }
+        })
+
+        
+    }
     return (
         <div>
             <section className='login'>
@@ -10,22 +41,22 @@ function Signin() {
                     <h2 className='title'>My Account</h2>
 
                     <span className='login__btns'>
-                        <button className='btn__signin'>Sign in</button>
-                        <button className='btn__signup' onClick={()=>navigate('/signup')}>Register</button>
+                        <button  className='btn__active'>Sign in</button>
+                        <button className='btn__signup' onClick={upRef}>Register</button>
                     </span>
 
-                    <form action="#" className='login__form'>
-                        <input type="email" placeholder='Email' className='login__email' />
-                        <input type="password" placeholder='Password' className='login__password' />
+                    <form onSubmit={fn} action="#" className='login__form'>
+                        <input type="text" placeholder='Email' className='login__email' name='text' />
+                        <input type="password" placeholder='Password' className='login__password' name='password' />
                         <span className='login__check'>
-                        <label  className='login__check'  htmlFor="chek">
-                        <input type="checkbox" id='chek' />
-                        <p>Remember me</p>
-                        </label>
-                        
+                            <label className='login__check' htmlFor="chek">
+                                <input type="checkbox" id='chek' />
+                                <p>Remember me</p>
+                            </label>
+
                         </span>
                         <button className='form__btn'>SIGN IN</button>
-                    <Link  className='link'>Have you forgotten your password?</Link>
+                        <Link className='link'>Have you forgotten your password?</Link>
                     </form>
                 </div>
             </section>
